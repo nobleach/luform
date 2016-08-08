27eb71b4df09edda355a82e99d167ca1c25fb8fc
Jim Wharton <jimwharton@overstock.com>
2016-08-08 15:53:38 -0600

    add User model, add passport

--- server/config/main.js
+++ server/config/main.js
@@ -1,3 +1,7 @@
 module.exports = {
+    // Secret key for JWT signing and encryption
+    'secret': 'VespaMyChild!',
+    // Database connection information
+    'database': 'mongodb://localhost:27017',
     'port': process.env.PORT || 4000
 }
